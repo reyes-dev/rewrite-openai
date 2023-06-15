a30e6822740af5d2534fcc274fb892c3e21c5197
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-06-15 04:15:48 -0700

    Refactor chatgpt method out of messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,15 +9,7 @@ class MessagesController < ApplicationController
   end
 
   def create
-    client = OpenAI::Client.new
-    prompt = "Rewrite the following text in the style of the author #{message_params[:author]} : #{message_params[:sent_body]}"
-    response = client.chat(
-    parameters: {
-        model: "gpt-3.5-turbo", 
-        messages: [{ role: "user", content: prompt}], 
-        temperature: 1,
-    })
-    response = response.dig("choices", 0, "message", "content") 
+    response = Message.chat_gpt_rewrite(params, message_params) 
     params[:message][:received_body] = response
     message = Message.create!(message_params)  
   end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,14 @@
 class Message < ApplicationRecord
+  def self.chat_gpt_rewrite(params, message_params)
+    client = OpenAI::Client.new
+    prompt = "Rewrite the following text in the style of the author #{message_params[:author]} : #{message_params[:sent_body]}"
+    response = client.chat(
+    parameters: {
+        model: "gpt-3.5-turbo", 
+        messages: [{ role: "user", content: prompt}], 
+        temperature: 1,
+    })
+    response = response.dig("choices", 0, "message", "content")
+    return response
+  end
 end
